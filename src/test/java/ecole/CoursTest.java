f7f5fe08829f4162a4d81258e9c9bf4b72d3fb25
sabrinamer <merdjoksabrina@gmail.com>
2019-12-05 15:13:13 +0100

    Ajout CoursTest

--- src/test/java/ecole/CoursTest.java
+++ src/test/java/ecole/CoursTest.java
@@ -1,4 +1,11 @@
 package ecole;
+import org.junit.Assert;
+import org.junit.Test;
+import point.Courbe;
+import point.Point;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.*;
 import org.junit.Test;
 import point.Courbe;
 import point.Point;
@@ -6,18 +13,40 @@ import point.Point;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import static org.junit.Assert.assertEquals;
 
 public class CoursTest {
 
-    @Test
-    private void addChapitretest(Chapitre ch){
-
-        Cours anglais=new Cours(new Ecole());
-
+    private boolean isInList (Chapitre chapitre, List<Chapitre> chapitres)
+    {
+        for (Chapitre chapitre1:chapitres)
+        {
+            if (chapitre.equals(chapitre1))
+                return true;
+        }
+        return false;
     }
-    private boolean comparerList(ArrayList<Chapitre> chapitres1, ArrayList<Chapitre> chapitres2){
-        return chapitres1.containsAll(chapitres2);
+    @Test
+    public void testAjouterChapitre(){
+        Cours cours = new Cours("Test",20);
+        Chapitre ch0 = new Chapitre("ch0",19);
+        cours.addChapitre(ch0);
+        List<Chapitre> chapitres = cours.getChapitres();
+        assertEquals(1, chapitres.size());
+        assertTrue(isInList(ch0, chapitres));
+        Chapitre ch1 = new Chapitre("ch1",2);
+        cours.addChapitre(ch1);
+        chapitres = cours.getChapitres();
+        assertEquals(1,chapitres.size());
+        assertFalse(isInList(ch1, chapitres));
+        Chapitre ch2 = null;
+        cours.addChapitre(ch2);
+        chapitres = cours.getChapitres();
+        assertEquals(1,chapitres.size());
+    }
+
+    private void assertFalse(boolean inList) {
     }
 
 }
